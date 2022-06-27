9b114319ca89a05401f98f5deb558aa178e8a875
Juanma <juanma@MacBook-JM.local>
2022-06-27 22:49:41 +0200

    Encoder

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 const files = require('./lib/files');
+const ffmpeg = require('./lib/ffmpeg');
 
-const filenames = files.getFiles('./files', 'flac');
-console.log(filenames);
+(async function() {
+    const filenames = files.getFiles('./files', 'flac');
+    console.log(filenames);
+
+    for (const f of filenames) {
+        const destination = await ffmpeg.encodeMp3(f);
+        console.log('Encoded:', destination);
+    }
+})();
