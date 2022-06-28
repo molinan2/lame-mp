4b4d6ec6d6722636bd5c2bb6898694518a281013
Juanma Molina <molinan2@gmail.com>
2022-06-28 15:27:27 +0200

    Reorganized libs

--- index.js
+++ index.js
@@ -1,29 +1,7 @@
-const files = require('./lib/files');
-const ffmpeg = require('./lib/ffmpeg');
-const Bottleneck = require('bottleneck/es5');
+const storage = require('./lib/storage');
+const encoding = require('./lib/encoding');
 
 (async function() {
-    const filenames = files.getFiles('./files', 'flac');
-    await batchEncode(filenames);
+    const filenames = storage.getFiles('./files', 'flac');
+    await encoding.batchEncodeMp3(filenames);
 })();
-
-/**
- * Batch encodes a list of files
- *
- * @param {Array.<string>} filenames
- * @returns {Promise}
- */
-async function batchEncode(filenames) {
-    const limiter = new Bottleneck({
-        maxConcurrent: 8,
-        minTime: 0
-    });
-
-    for (const f of filenames) {
-        limiter.schedule(() => ffmpeg.encodeMp3(f));
-    }
-
-    return new Promise((resolve, reject) => {
-        limiter.on('idle', () => resolve());
-    })
-}
