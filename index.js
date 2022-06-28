4bd04ec5bdee90fff3b67768ee30490314507e00
Juanma Molina <molinan2@gmail.com>
2022-06-29 01:39:56 +0200

    Improved filtering and automatic concurrency

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ const storage = require('./lib/storage');
 const encoding = require('./lib/encoding');
 
 (async function() {
-    const filenames = storage.getFiles('./files', 'flac');
+    const folder = './files';
+    const extensions = ['flac', 'wav', 'm4a', 'aac'];
+    const filenames = storage
+        .getFilenames(folder)
+        .filter(f => isFilenameAllowed(f,extensions));
     await encoding.batchEncodeMp3(filenames);
 })();
+
+function isFilenameAllowed(filename, extensions=null) {
+    if (!extensions) return true;
+    for (const e of extensions) {
+        if (filename.endsWith(`.${e}`)) return true;
+    }
+    return false;
+}
