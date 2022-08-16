1ae4c448307a0b0cfcf04875c467612b144d6d80
Juanma Molina <molinan2@gmail.com>
2022-08-16 12:38:51 +0200

    Added m4a, Renamed

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const encoding = require('./lib/encoding');
     const extensions = ['flac', 'wav', 'm4a', 'aac'];
     const filenames = storage
         .getFilenames(folder)
-        .filter(f => isFilenameAllowed(f,extensions));
+        .filter(f => isFileTypeAllowed(f,extensions));
     await encoding.batchEncodeMp3(filenames, 0);
 })();
 
-function isFilenameAllowed(filename, extensions=null) {
+function isFileTypeAllowed(filename, extensions=null) {
     if (!extensions) return true;
     for (const e of extensions) {
         if (filename.endsWith(`.${e}`)) return true;
